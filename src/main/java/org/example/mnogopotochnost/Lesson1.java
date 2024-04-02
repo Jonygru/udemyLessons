ae69791ac29c6e5f9a369f35abb66ea0a7b9de1f
Asus <jonygru92@yandex.ru>
2024-04-02 20:25:17 +0300

    12344

--- src/main/java/org/example/mnogopotochnost/Lesson1.java
+++ src/main/java/org/example/mnogopotochnost/Lesson1.java
@@ -3,7 +3,6 @@ package org.example.mnogopotochnost;
 public class Lesson1 {
     public void foo(){
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 
 }
